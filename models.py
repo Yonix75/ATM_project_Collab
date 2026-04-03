908d683b84ae2495c7d6f47289ff36e83fe20340
Yoni <watch.black900@gmail.com>
2026-04-03 19:16:13 +0300

    working Auth()

--- models.py
+++ models.py
@@ -1,60 +1,39 @@
 from datetime import *
 
 
-
-
-
 class Bank:
-    def __init__(self):
+    def init(self):
         pass
-    
-    
+
+
 class Account:
-    def __init__(self, id, name ,pin ,balance, is_activated, transaction ):
+    def init(self, id, name ,pin ,balance, is_activated, transaction ):
         self.id = id
         self.name=name
         self.balance=balance
-        self.pin = str(pin)
-        self.is_activated = False
-        self.transaction = [ ]
+        self.pin = pin
+        self.is_activated = is_activated
+        self.transaction = transaction
+
+    def auth(self):
+        id_client = (input("Enter your account ID: "))   # קבלת מזהה מהמשתמש
+        pin_client = (input("enter your PIN: "))       # קבלת סיסמא מהמשתמש
+
+        if id_client == self.id and pin_client == self.pin:
+            self.is_activated = True
+            print("correct")
+            return self.id
+
+        else:
+            print("is Incorrect")
+            self.is_activated = False
 
 
-        
-acount1 =Account(123,"niv", 500, "123", False, [])
+
+acount1 =Account("123","niv", "123", 500, False, [])
 
 
 
-        
-id_client = int(input("Enter your account id: "))
-pin_client = int(input("enter your PIN: "))
-    
-def auth(self):
-    
-    id_client = int(input("Enter your account id"))   # קבלת מזהה מהמשתמש
-    pin_client = int(input("enter your PIN: "))       # קבלת סיסמא מהמשתמש
 
-    if id_client == self.id and pin_client == self.pin:
-        self.is_activated = True
-        print("correct")
-        return self.id
-        
-    else:
-        print("is Incorrect")
-            
 def addTransaction():
     pass
-    
-
-
-auth(acount1)
-
-                
-            
-            
-            
-            
-     
-     
-     
-    
-     
