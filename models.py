13e2ae03be429cbb1d48a0a0c22a764b856f77d4
NivBar2000 <bar.niv2000@gmail.com>
2026-04-03 18:33:15 +0300

    changes in models.py

--- models.py
+++ models.py
@@ -1,34 +1,53 @@
 from datetime import *
 
+
+
+
+
 class Bank:
     def __init__(self):
         pass
     
     
 class Account:
-    def __init__(self,pin,name,passwrd,money):
-        self.pin=pin
+    def __init__(self, id, name ,pin ,balance, is_activated, transaction ):
+        self.id = id
         self.name=name
-        self.money=money
-        self.passwrd = str(passwrd)
-        self.is_activated = True
+        self.balance=balance
+        self.pin = str(pin)
+        self.is_activated = False
         self.transaction = [ ]
+
+
         
+acount1 =Account(123,"niv", 500, "123", False, [])
+
+
+
         
+id_client = int(input("Enter your account id: "))
+pin_client = int(input("enter your PIN: "))
+    
+def auth(self):
+    
+    id_client = int(input("Enter your account id"))   # קבלת מזהה מהמשתמש
+    pin_client = int(input("enter your PIN: "))       # קבלת סיסמא מהמשתמש
+
+    if id_client == self.id and pin_client == self.pin:
+        self.is_activated = True
+        print("correct")
+        return self.id
         
-        
-    def auth(self,pin):
-        if self.pin == pin:
-           self.is_activated = True
-           return self.pin
-        else:
-            print("is Incorrect")
+    else:
+        print("is Incorrect")
             
-    def addTransaction():
-        pass
-        
-        
-        
+def addTransaction():
+    pass
+    
+
+
+auth(acount1)
+
                 
             
             
